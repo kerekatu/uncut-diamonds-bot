a32d5e4c58d3df585c429cb28298ae08edbb5590
KR <grplayeer@gmail.com>
2022-01-18 11:38:35 +0100

    feat: alko command

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Client } from 'discord.js'
+import { ActivitiesOptions, Client } from 'discord.js'
 import config from './config'
 import * as commandModules from './commands'
 import { Command } from './types'
@@ -6,11 +6,35 @@ import { Command } from './types'
 const commands: { [key: string]: Command } = Object(commandModules)
 
 export const client = new Client({
-  intents: ['GUILDS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES'],
+  intents: [
+    'GUILDS',
+    'GUILD_MESSAGES',
+    'DIRECT_MESSAGES',
+    'GUILD_MESSAGE_REACTIONS',
+  ],
 })
 
-client.once('ready', () => {
+client.once('ready', async () => {
   console.log('Discord bot initialized!')
+
+  const guild = await client.guilds.fetch(`${process.env.GUILD_ID}`)
+
+  const list: { activity: string; type: ActivitiesOptions['type'] }[] = [
+    { activity: 'zabawa kuleczkami bucza', type: 'PLAYING' },
+    { activity: 'najebany już jak ściana', type: 'LISTENING' },
+    { activity: 'przywołuje demona hybrydę', type: 'STREAMING' },
+    { activity: '/alko', type: 'PLAYING' },
+    { activity: `wraz z ${guild.memberCount} diamentami`, type: 'PLAYING' },
+  ]
+  let index = 0
+  setInterval(() => {
+    if (index < list.length) index++
+    if (index === list.length) index = 0
+
+    client.user?.setActivity(list[index].activity, {
+      type: list[index].type,
+    })
+  }, 1000 * 60 * 30)
 })
 
 client.on('interactionCreate', async (interaction) => {
--- src/commands/alko/index.ts
+++ src/commands/alko/index.ts
@@ -1,34 +1,196 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
-import { CommandInteraction, MessageEmbed } from 'discord.js'
+import {
+  CommandInteraction,
+  Message,
+  MessageActionRow,
+  MessageButton,
+  MessageComponentInteraction,
+  MessageEmbed,
+  MessageReaction,
+  User,
+} from 'discord.js'
 import { CONSTANTS } from '../../utils/constants'
+import { PrismaClient } from '@prisma/client'
 
-const example = [
-  { title: 'Żubr Ciemnozłoty', link: 'upload/zubr_ciemnozloty.webp' },
-  {
-    title: 'Lubuskie Miodowe Jasne',
-    link: 'upload/1ec8c4fa012b53cf06f4ae01f38a49de.webp',
-  },
-  { title: 'Paulaner Salvator', link: 'upload/paulaner_salvator.webp' },
-  {
-    title: 'Senojo Vilniaus Tamsusis Alus',
-    link: 'upload/senojo_vilniaus_tamsusis.webp',
-  },
-  { title: 'Fullers Discovery', link: 'upload/discovery_straight.webp' },
-  { title: 'La Trappe Dubbel', link: 'upload/la_trappe_dubbel.webp' },
-  { title: 'Wojak Jasny Pełny 6%', link: 'upload/wojak.webp' },
-]
+const prisma = new PrismaClient()
+const recentlyUsed = new Set()
 
 export const data = new SlashCommandBuilder()
   .setName('alko')
   .setDescription('Gra w zbieranie trunków')
+  .addUserOption((option) =>
+    option.setName('gracz').setDescription('Wybierz gracza')
+  )
 
 export async function execute(interaction: CommandInteraction) {
-  const randomItem = example[Math.floor(Math.random() * example.length)]
-  const embed = new MessageEmbed()
-    .setColor(CONSTANTS.color)
-    .setTitle(randomItem.title)
-    .setImage(`${CONSTANTS.URL_BEERS}/${randomItem.link}`)
-  await interaction.reply({
-    embeds: [embed],
-  })
+  const user = interaction.options.getUser('gracz')
+
+  if (user) {
+    let currentPage = 1
+    const itemsPerPage = 5
+    const buttonPreviousId = 'previous'
+    const buttonNextId = 'next'
+
+    const productsCount = await prisma.alcohols.count({
+      where: { userId: user.id },
+    })
+    const numberOfPages = Math.ceil(productsCount / itemsPerPage)
+
+    const previousButton = new MessageButton()
+      .setCustomId(buttonPreviousId)
+      .setLabel('Poprzednia')
+      .setStyle('PRIMARY')
+
+    const nextButton = new MessageButton()
+      .setCustomId(buttonNextId)
+      .setLabel('Następna')
+      .setStyle('PRIMARY')
+
+    const generateEmbed = async () => {
+      const products = await prisma.alcohols.findMany({
+        skip: currentPage === 1 ? 0 : (currentPage - 1) * itemsPerPage,
+        take: 5,
+        where: {
+          userId: user.id,
+        },
+      })
+
+      return new MessageEmbed()
+        .setColor(CONSTANTS.color)
+        .setAuthor({
+          name: `${user.username}`,
+          iconURL: user.displayAvatarURL(),
+        })
+        .addField(
+          'LISTA GRACZA • TRUNKI',
+          `${products
+            .map(
+              (item, index) =>
+                `**${index + 1 + (currentPage - 1) * itemsPerPage}.** ${
+                  item.title
+                }\n`
+            )
+            .join(' ')}`
+        )
+        .setFooter({
+          text: `Strona ${currentPage}/${numberOfPages} • ${productsCount} trunków`,
+        })
+    }
+
+    await interaction.reply({
+      embeds: [await generateEmbed()],
+      components: [
+        new MessageActionRow({
+          components: [
+            previousButton.setDisabled(currentPage === 1),
+            nextButton.setDisabled(currentPage === numberOfPages),
+          ],
+        }),
+      ],
+    })
+
+    const filter = (i: MessageComponentInteraction) =>
+      interaction.user.id === i.user.id &&
+      (i.customId === buttonNextId || i.customId === buttonPreviousId)
+
+    const collector = interaction.channel?.createMessageComponentCollector({
+      filter,
+      componentType: 'BUTTON',
+      time: 1000 * 60,
+    })
+
+    collector?.on('collect', async (i) => {
+      if (i.customId === buttonNextId) {
+        if (currentPage < numberOfPages) currentPage++
+        await i.update({
+          embeds: [await generateEmbed()],
+          components: [
+            new MessageActionRow({
+              components: [
+                previousButton.setDisabled(currentPage === 1),
+                nextButton.setDisabled(currentPage === numberOfPages),
+              ],
+            }),
+          ],
+        })
+      } else if (i.customId === buttonPreviousId) {
+        if (currentPage > 1) currentPage--
+        await i.update({
+          embeds: [await generateEmbed()],
+          components: [
+            new MessageActionRow({
+              components: [
+                previousButton.setDisabled(currentPage === 1),
+                nextButton.setDisabled(currentPage === numberOfPages),
+              ],
+            }),
+          ],
+        })
+      }
+    })
+  } else {
+    const emoji = '❤️'
+    const productsCount = await prisma.alcohols.count()
+    const randomItem = await prisma.alcohols.findUnique({
+      where: {
+        id: Math.floor(Math.random() * productsCount),
+      },
+    })
+
+    if (!randomItem)
+      return await interaction.reply({ content: 'Coś poszło nie tak...' })
+
+    const embed = new MessageEmbed()
+      .setColor(CONSTANTS.color)
+      .setTitle(randomItem.title)
+      .setImage(`${CONSTANTS.URL_BEERS}/${randomItem.link}`)
+
+    if (recentlyUsed.has(interaction.member?.user.id)) {
+      interaction.reply('Poczekaj minutę przed następnym użyciem...')
+    } else {
+      if (randomItem.userId) {
+        embed.addField(`\u200b`, `Należy do <@${randomItem.userId}>`)
+        await interaction.reply({
+          embeds: [embed],
+        })
+      } else {
+        const message = (await interaction.reply({
+          embeds: [embed],
+          fetchReply: true,
+        })) as Message
+        await message.react(emoji)
+
+        const filter = (reaction: MessageReaction, user: User): boolean =>
+          reaction.emoji.name === emoji &&
+          user.id === message.interaction?.user.id
+
+        const collector = message.createReactionCollector({
+          filter,
+          time: 1000 * 15,
+          maxEmojis: 2,
+          max: 1,
+        })
+        collector.on('collect', async (i) => {
+          await prisma.alcohols.update({
+            where: {
+              id: randomItem.id,
+            },
+            data: {
+              userId: interaction.user.id,
+            },
+          })
+          await interaction.editReply({
+            embeds: [
+              embed.addField(`\u200b`, `Należy do <@${interaction.user.id}>`),
+            ],
+          })
+        })
+      }
+
+      recentlyUsed.add(interaction.user.id)
+      setTimeout(() => {
+        recentlyUsed.delete(interaction.user.id)
+      }, 60000)
+    }
+  }
 }
