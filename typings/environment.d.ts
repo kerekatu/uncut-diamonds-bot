974355a4c6e406f0e0c3d57d92ef7bb10f7237a5
Konrad Rosa <40122362+kerekatu@users.noreply.github.com>
2022-03-24 13:34:49 +0100

    feat(typings): env types

---
+++ typings/environment.d.ts
@@ -0,0 +1,17 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      BOT_TOKEN: string
+      APP_ID: string
+      GUILD_ID: string
+      UNBELIEVABOAT_TOKEN: string
+      DATABASE_URL: string
+      EMBED_COLOR: string
+      ALCO_COOLDOWN: number
+      ACTIVITIES: string[]
+      ACTIVITY_TYPES: string[]
+    }
+  }
+}
+
+export {}
